e52fa7e4477328c1e13eb09db9327ad89ee33e48
jttrevis <jr.trevis@hotmail.com>
2023-02-03 22:47:40 +0000

    cards

--- src/components/CardsProjects/index.jsx
+++ src/components/CardsProjects/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.scss';
 import gitLogo from '../../assets/logos/github.png';
 import screenLogo from '../../assets/logos/screen.svg';
@@ -7,6 +7,12 @@ import djSite from '../../assets/portifolioImages/djSite.jpg';
 import vetClinic from '../../assets/portifolioImages/vetClinic.jpg';
 
 export const CardProjects = () => {
+	const [flip, setFlip] = useState('cardContentBack');
+
+	const handleFlip = () => {
+		setFlip('cardContentBack');
+	};
+
 	return (
 		<>
 			<div
@@ -29,31 +35,18 @@ export const CardProjects = () => {
 						<div className='cardContentBack'>
 							<div className='cardBackText'>
 								<p>
-									<h3>About:</h3>
-									This project simulates a Pizzeria website, where you can
+									<h3>About: Pizzaria "E-commerce"</h3>
+									This project simulates a Pizzaria website, where you can
 									choose between different product options.
 								</p>
-								<ul>
-									<h3>Features:</h3>
-									<li>Navigate through the navbar on multiple pages.</li>
-									<li>
-										Add and remove different types of products on cart shop
-									</li>
-									<li>Check the localization on Google Maps.</li>
-								</ul>
 
-								<ul>
-									<h3>Technology:</h3>
-									<li>HTML </li>
-									<li>CSS</li>
-									<li>Material UI</li>
-									<li>Styled Components</li>
-									<li>JavaScript</li>
-									<li>React JS</li>
-								</ul>
+								<h3>Technology:</h3>
 								<p>
-									<h3>Deploy:</h3>
-									Vercel
+									HTML | CSS | MaterialUI | Styled Components | JavaScript |
+									ReactJS | ContextAPI
+								</p>
+								<p>
+									<p>Deploy: Vercel</p>
 								</p>
 							</div>
 							<div>
@@ -101,31 +94,18 @@ export const CardProjects = () => {
 						<div className='cardContentBack'>
 							<div className='cardBackText'>
 								<p>
-									<h3>About:</h3>
+									<h3>About: "Veterinary Clinic"</h3>
 									The project is a responsive veterinary clinic website that has
-									been designed with a mobile-first approach.
+									been designed with a mobile-first approach. Users can even
+									register and their data is directly saved to Firebase for easy
+									accessibility. The styling of the website was done using
+									Tailwind CSS which provides a sleek and modern look.
 								</p>
+								<h3>Technology:</h3>
 								<p>
-									The website features sections for information about the
-									clinic, services offered, and the ability to schedule
-									appointments. Users can even register and their data is
-									directly saved to Firebase for easy accessibility. The styling
-									of the website was done using Tailwind CSS which provides a
-									sleek and modern look.
+									HTML | Firebase | Tailwind | EmailJs | JavaScript | ReactJS
 								</p>
-								<ul>
-									<h3>Technology:</h3>
-									<li>HTML </li>
-									<li>Firebase</li>
-									<li>Tailwind CSS</li>
-									<li>Email Js</li>
-									<li>JavaScript</li>
-									<li>React JS</li>
-								</ul>
-								<p>
-									<h3>Deploy:</h3>
-									Vercel
-								</p>
+								<p>Deploy: Vercel</p>
 							</div>
 							<div>
 								<div className='buttons'>
@@ -172,29 +152,15 @@ export const CardProjects = () => {
 						<div className='cardContentBack'>
 							<div className='cardBackText'>
 								<p>
-									<h3>About:</h3>
+									<h3>About: "institutional website"</h3>
 									This project i build using mobile first method for my brother
 									in Brazil to use in your business working as DJ.
 								</p>
-								<ul>
-									<h3>Features:</h3>
-									<li>Navigate through the navbar on multiple pages.</li>
-									<li>Contact through the website by email and social media</li>
-									<li>Image and Videos gallery.</li>
-								</ul>
 
-								<ul>
-									<h3>Technology:</h3>
-									<li>HTML</li>
-									<li>Email Js</li>
-									<li>SASS</li>
-									<li>JavaScript</li>
-									<li>React JS</li>
-								</ul>
-								<p>
-									<h3>Deploy:</h3>
-									Vercel
-								</p>
+								<h3>Technology:</h3>
+								<p>HTML | SASS | EmailJs | JavaScript | ReactJS</p>
+
+								<p>Deploy: Vercel</p>
 							</div>
 							<div>
 								<div className='buttons'>
